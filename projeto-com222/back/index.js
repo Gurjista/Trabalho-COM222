14b471c107343cf7e268f59c063436042ea619f8
Gustavo Rocha <gustavo.prietorocha1904@gmail.com>
2023-12-07 20:23:01 -0300

    Add base backend

---
+++ projeto-com222/back/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+//Necessário para extrair os dados de Forms vindos de uma requisição POST
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(3000, () => {
+    console.log('Servidor na porta 3000');
+});
---
+++ projeto-com222/back/model/User.js
@@ -0,0 +1,11 @@
+class User {
+
+    constructor(id, username, email,password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+  }
+  
+  module.exports = User;
