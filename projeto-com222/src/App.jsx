8e916c24e13c5564e98b3947ee10ca0f7022815e
Gustavo Rocha <gustavo.prietorocha1904@gmail.com>
2023-12-08 17:15:18 -0300

    movi a pasta login pra dentro do src

--- projeto-com222/src/App.jsx
+++ projeto-com222/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
