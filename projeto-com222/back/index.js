aac2cb3e280f5b07a9bfe1dcc590a8540d422903
Gustavo Rocha <gustavo.prietorocha1904@gmail.com>
2023-12-08 19:16:18 -0300

    FIX CreateUser e index.js

--- projeto-com222/back/index.js
+++ projeto-com222/back/index.js
@@ -18,58 +18,68 @@ app.listen(3000, () => {
 
 const User = require('./model/User');
 
+
+
 app.post('/login', async (req,res) => {
+    //extraindo os dados do formulário para criacao do usuario
+    const {email, password} = req.body; 
     
-    const {email, password} = req.body;
+    //Abre o bd (aqui estamos simulando com arquivo)
+    const jsonPath = path.join(__dirname, '.', 'db', 'banco-dados-usuario.json');
+    const usuariosCadastrados = JSON.parse(fs.readFileSync(jsonPath, { encoding: 'utf8', flag: 'r' }));
 
-    const jsonPath = path.join(__dirname, '.', 'db','banco-dados-usuario.json');
-    const usuariosCadastrados = JSON.parse(fs.readFileSync(jsonPath, {enconding : 'utf8', flag: 'r'}));
-
-    
+    //verifica se existe usuario com email    
     for (let user of usuariosCadastrados){
         if(user.email === email){
             const passwordValidado = await bcrypt.compare(password, user.password);
-            if(passwordValidado){
+            if(passwordValidado){ 
+                
                 const token = jwt.sign(user, process.env.TOKEN);
 
-                return res.json({"token" : token});
+                return res.json({ "token" : token});
             }
-
+            
             else
                 return res.status(422).send(`Usuario ou senhas incorretas.`);
-        }
-
+        }   
     }
-});
+    //Nesse ponto não existe usuario com email informado.
+    return res.status(409).send(`Usuario com email ${email} não existe. Considere criar uma conta!`);
 
-app.get('/create', async (req,res) => {
-    //extraçao dos dados do formulario para criara o usuario
-    const {username, email, password} = req.body;
+})
 
-    const jsonPath = path.join(__dirname,'.', 'db', 'banco-dados-usuario.json');
-    const usuariosCadastrados = JSON.parse(fs.readFileSync(jsonPath, {encoding: 'utf-8', flag: 'r'}));
+app.post('/create', async (req,res) => {
+    //extraindo os dados do formulário para criacao do usuario
+    const {username, email, password} = req.body; 
+    
+    const jsonPath = path.join(__dirname, '.', 'db', 'banco-dados-usuario.json');
+    const usuariosCadastrados = JSON.parse(fs.readFileSync(jsonPath, { encoding: 'utf8', flag: 'r' }));
 
-    //verificaçao de se o usuario ja existe com o email cadastrado
-    for(let users of usuariosCadastrados){
-        if(users.email === emaill){
-            //usuario existente, impossivel criar outro
-            //retornando o erro 409 para indicar o conflito
-            return res.status(409).send(`Usuario com email ${email} já existente`);
-        }
-        const id = usuariosCadastrados.length + 1;
-
-        //geraçao de senha criptografada
-        const salt = await bcrypt.genSalt(10);
-        const passwordCrypt = await bcrypt.hash(password,salt);
-
-        //criando user
-        const user = new User(id, username, email, passwordCrypt);
-
-        //salvando usuario no "banco"
-        usuariosCadastrados.push(user);
-        fs.writeFileSync(express.json,JSON.stringify(usuariosCadastrados,null,2));
-        res.send(`Tudo certo usuario criado com sucesso.`);
+    //verifica se já existe usuario com o email informado
+    
+    for (let users of usuariosCadastrados){
+        if(users.email === email){
+            //usuario já existe. Impossivel criar outro
+            //Retornando o erro 409 para indicar conflito
+            return res.status(409).send(`Usuario com email ${email} já existe.`);
+        }   
     }
+    //Deu certo. Vamos colocar o usuário no "banco"
+    //Gerar um id incremental baseado na qt de users
+    const id = usuariosCadastrados.length + 1;
+    
+
+    //gerar uma senha cryptografada
+    const salt = await bcrypt.genSalt(10);
+    const passwordCrypt = await bcrypt.hash(password,salt);
+
+    //Criacao do user
+    const user = new User(id, username, email, passwordCrypt);
+
+    //Salva user no "banco"
+    usuariosCadastrados.push(user);
+    fs.writeFileSync(jsonPath,JSON.stringify(usuariosCadastrados,null,2));
+    res.send(`Tudo certo usuario criado com sucesso.`);
 });
 
 function verificaToken(req,res,next){
--- projeto-com222/src/login/CreateUser.jsx
+++ projeto-com222/src/login/CreateUser.jsx
@@ -1,24 +1,38 @@
-import { useForm } from 'react-hook-form';
+import {set, useForm} from 'react-hook-form';
 import axios from 'axios';
-import { useSate, useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function CreateUser() {
+export default function CreateUser(){
 
     const [msg, setMsg] = useState();
-    const [userCriado, setUserCriado] = useState(false);
-    const form = useForm();
+    const [userCriado,setUserCriado] = useState(false);
 
-    const { register, control, handleSubmit, formState } = form;
+    const schema = yup.object({
+        username: yup.string().required('Usuário obrigatório'),
+        email: yup.string().email('Email inválido').required('Email obrigatório'),
+        password: yup.string().min(2,'Senha com no mínimo 2 caracteres').required(),
+        passwordConf: yup.string().required('Confirme a senha').oneOf([yup.ref('password')], 'As senhas devem coincidir!'),
+    });
 
-    const { errors } = formState;
+    const form = useForm({
+        resolver: yupResolver(schema)
+    });
+
+    
+
+    const { register, handleSubmit, formState } = form;
+
+    const {errors} = formState;
 
     const submit = async (data) => {
 
         try {
             const response = await axios.post('http://localhost:3000/create', data);
             setMsg(response.data);
-            if (response.data.includes('sucesso'))
+            if(response.data.includes('sucesso'))
                 setUserCriado(true);
         } catch (error) {
             setMsg(error.response.data);
@@ -49,6 +63,9 @@ export default function CreateUser() {
             </form>
 
             <p className='server-response'>{msg}</p>
+            <Link to="/"
+            style={{visibility : userCriado ? 'visible' : 'hidden' }}
+            >Fazer Login</Link>
             
 
         </>
